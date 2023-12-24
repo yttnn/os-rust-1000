112dba3554d0bc078077b8dac353e8ffd0c7a461
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-24 12:07:25 +0900

    bss zero clear

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 #![no_std]
 #![no_main]
 
+use core::{arch::asm, ptr::write_bytes};
+
+extern "C" {
+  static __stack_top: u8;
+  static mut __bss: u8;
+  static __bss_end: u8;
+}
 
 #[no_mangle]
 #[link_section = ".text.boot"]
-pub extern  "C" fn boot() -> ! {
+pub unsafe extern  "C" fn boot() -> ! {
+  asm!(
+    "mv sp, {stack_top}",
+    "j {kernel_main}",
+    stack_top = in(reg) &__stack_top,
+    kernel_main = sym kernel_main,
+  );
+  loop {}
+}
+
+#[no_mangle]
+fn kernel_main() -> ! {
+  unsafe {
+    let addr_bss_start = &__bss as *const u8;
+    let addr_bss_end = &__bss_end as *const u8;
+    let bss_size = addr_bss_end as usize - addr_bss_start as usize;
+    write_bytes(&mut __bss as *mut u8, 0, bss_size);
+  }
   loop {}
 }
 
